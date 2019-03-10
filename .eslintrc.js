6e3ae7da7be4215c6aa51e4728a16159f181b167
WDever <alsrbtkfkd7@gmail.com>
2019-03-10 23:40:10 +0900

    Configure Eslint + Prettier Configured Eslint + Prettier to lint typescript. Added VScode configuration to lint .ts and .tsx files. Added classnames module for easy use of .module.scss.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    "@typescript-eslint/indent": ["error", 2],
+    // "@typescript-eslint/explicit-function-return-type": ["false"],
+    "@typescript-eslint/explicit-function-return-type": 0,
+    // "prettier/prettier": ["error", {
+    //   "singleQuote": true,
+    //   "parser": "flow"
+    // }],
+    // "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": true }],
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-empty-interface": 0,
+    "prettier/prettier": 0,
+    // "@typescript-eslint/no-unused-vars": 0,
+    // "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+  },
+};
