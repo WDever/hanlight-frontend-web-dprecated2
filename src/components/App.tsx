c1709a362e6a5e73749dfd8a5b966e40ea4958c0
WDever <alsrbtkfkd7@gmail.com>
2019-04-11 23:00:20 +0900

    Prepare to use redux and redux-saga Add font

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,18 +2,20 @@ import * as React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import AuthComponent from '../components/auth';
+import AuthPage from '../pages/auth';
 import HomePage from '../pages/home';
-import PostFormTest from './postForm';
-import SignComponent from './sign';
+// import PostFormTest from './postForm';
+import RegisterComponent from './auth/register';
+import TEst from '../components/sign';
 
 const AppComponent: React.SFC<{}> = () => {
   return (
     <div id='app' lang='ko'>
       <Switch>
-        <Route path='/auth' component={AuthComponent} />
-        {/* <Route path='/login' component={LoginComponent} /> */}
-        <Route path='/sign' component={SignComponent} />
-        <Route exact path='/' component={HomePage} />
+        <Route path='/auth/register' component={RegisterComponent} />
+        <Route path='/auth' component={AuthPage} />
+        <Route path='/test' component={TEst} />
+        <Route path='/' component={HomePage} />
         <Redirect to='/' />
       </Switch>
     </div>
